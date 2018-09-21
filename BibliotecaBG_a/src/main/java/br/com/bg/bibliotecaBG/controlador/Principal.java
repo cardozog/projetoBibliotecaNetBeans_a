a1ca86b14d38eeb657230a7b256b43f627381267
2B Modular Info <2binfo@basilides.local>
2018-09-20 22:28:51 -0300

    Criação da classe principal e colocação de botão com imagem

---
+++ BibliotecaBG_a/src/main/java/br/com/bg/bibliotecaBG/controlador/Principal.java
@@ -0,0 +1,17 @@
+package br.com.bg.bibliotecaBG.controlador;
+
+import br.com.bg.bibliotecaBG.tela.TelaMenu;
+
+/**
+ *
+ * @author Guilherme C
+ * @since 20/09/2018
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        TelaMenu telaMenu = new TelaMenu();
+        telaMenu.setVisible(true);
+
+    }
+}
